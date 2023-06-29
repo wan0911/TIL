a37a54ed357f4584e8561f36cc89f55901564dd6
wan <2jin_j@naver.com>
2023-06-29 17:35:57 +0900

    0629

---
+++ C/etc/debug.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int arr[10];
+  int i;
+
+  for (i = 0; i < 10; i++) {
+    scanf("%d", &arr[i]);
+  }
+  for (i = 0; i <= 10; i++) {
+    printf("%d \n", arr[i]);
+  }
+  return 0;
+}
--- README.md
+++ README.md
@@ -11,6 +11,6 @@
 <br/>
 
 ### C
-
+[C 노션 정리](https://spot-grin-55d.notion.site/0b5319d48e7743368f00735b1044e226?pvs=4)
 
 
